1160749bd4505b49a6ed2952f9bccce3e1565a38
Owen Sowatzke <osowatzke@gmail.com>
2025-04-26 03:02:07 -0700

    Trying to hook up FPU in top-level design

--- picorv32.v
+++ picorv32.v
@@ -264,6 +264,11 @@ module picorv32 #(
 	wire        pcpi_div_wait;
 	wire        pcpi_div_ready;
 
+    wire        pcpi_fpu_wr;
+	wire [31:0] pcpi_fpu_rd;
+	wire        pcpi_fpu_wait;
+	wire        pcpi_fpu_ready;
+    
 	reg        pcpi_int_wr;
 	reg [31:0] pcpi_int_rd;
 	reg        pcpi_int_wait;
@@ -321,12 +326,32 @@ module picorv32 #(
 		assign pcpi_div_wait = 0;
 		assign pcpi_div_ready = 0;
 	end endgenerate
+    
+    generate if (ENABLE_FPU) 
+        fmuls (
+            .clkIn       (clk            );
+            .rstLowIn    (resetn         );
+            .pcipValidIn (pcpi_valid     );
+            .pcipInstIn  (pcpi_insn      );
+            .pcipRs1In   (pcpi_rs1       );
+            .pcipRs2In   (pcpi_rs2       );
+            .pcipWrIn    (pcpi_fpu_wr    );
+            .pcipRdOut   (pcpi_fpu_rd    );
+            .pcipWaitOut (pcpi_fpu_wait  );
+            .pcipReadyOut(pcpi_fpu_ready )
+        );
+    end else begin
+        assign pcpi_fpu_wr = 0;
+		assign pcpi_fpu_rd = 32'bx;
+		assign pcpi_fpu_wait = 0;
+		assign pcpi_fpu_ready = 0;
+    end endgenerate
 
 	always @* begin
 		pcpi_int_wr = 0;
 		pcpi_int_rd = 32'bx;
-		pcpi_int_wait  = |{ENABLE_PCPI && pcpi_wait,  (ENABLE_MUL || ENABLE_FAST_MUL) && pcpi_mul_wait,  ENABLE_DIV && pcpi_div_wait};
-		pcpi_int_ready = |{ENABLE_PCPI && pcpi_ready, (ENABLE_MUL || ENABLE_FAST_MUL) && pcpi_mul_ready, ENABLE_DIV && pcpi_div_ready};
+		pcpi_int_wait  = |{ENABLE_PCPI && pcpi_wait,  (ENABLE_MUL || ENABLE_FAST_MUL) && pcpi_mul_wait,  ENABLE_DIV && pcpi_div_wait,  ENABLE_FPU && pcpi_fpu_wait};
+		pcpi_int_ready = |{ENABLE_PCPI && pcpi_ready, (ENABLE_MUL || ENABLE_FAST_MUL) && pcpi_mul_ready, ENABLE_DIV && pcpi_div_ready, ENABLE_FPU && pcpi_fpu_ready};
 
 		(* parallel_case *)
 		case (1'b1)
@@ -341,6 +366,10 @@ module picorv32 #(
 			ENABLE_DIV && pcpi_div_ready: begin
 				pcpi_int_wr = pcpi_div_wr;
 				pcpi_int_rd = pcpi_div_rd;
+			end
+            ENABLE_FPU && pcpi_fpu_ready: begin
+				pcpi_int_wr = pcpi_fpu_wr;
+				pcpi_int_rd = pcpi_fpu_rd;
 			end
 		endcase
 	end
@@ -2530,6 +2559,7 @@ module picorv32_axi #(
 	parameter [ 0:0] ENABLE_MUL = 0,
 	parameter [ 0:0] ENABLE_FAST_MUL = 0,
 	parameter [ 0:0] ENABLE_DIV = 0,
+    parameter [ 0:0] ENABLE_FPU = 0,
 	parameter [ 0:0] ENABLE_IRQ = 0,
 	parameter [ 0:0] ENABLE_IRQ_QREGS = 1,
 	parameter [ 0:0] ENABLE_IRQ_TIMER = 1,
@@ -2925,6 +2955,7 @@ module picorv32_wb #(
 		.ENABLE_MUL          (ENABLE_MUL          ),
 		.ENABLE_FAST_MUL     (ENABLE_FAST_MUL     ),
 		.ENABLE_DIV          (ENABLE_DIV          ),
+        .ENABLE_FPU          (ENABLE_FPU          ),
 		.ENABLE_IRQ          (ENABLE_IRQ          ),
 		.ENABLE_IRQ_QREGS    (ENABLE_IRQ_QREGS    ),
 		.ENABLE_IRQ_TIMER    (ENABLE_IRQ_TIMER    ),
