e9d417cbbc8b6e452a0227f94afb76c89612a2e1
khadkaxashish <khadkaxashish@gmail.com>
2025-05-02 14:40:45 -0700

    show layout after build

--- picorv32.py
+++ picorv32.py
@@ -4,11 +4,12 @@
 import siliconcompiler
 from siliconcompiler.targets import skywater130_demo
 
-chip = siliconcompiler.Chip('picorv32')     # create chip object
-chip.load_target('skywater130_demo')        # load predefined target
-chip.input('picorv32.v')                    # define list of source files
-chip.clock('clk', period=10)                # define clock speed of design
-chip.set('option', 'remote', True)          # run remote in the cloud
-chip.run()                                  # run chip compilation
-chip.summary()                              # print results summary with PNG screenshot and HTML report
-
+if __name__ == "__main__":
+    chip = siliconcompiler.Chip('picorv32')   # create chip object
+    chip.input('picorv32.v')                  # define list of source files
+    chip.clock('clk', period=10)              # define clock speed of design
+    chip.use(skywater130_demo)                # load predefined technology and flow target
+    chip.set('option', 'remote', True)        # run remote in the cloud
+    chip.run()                                # run compilation of design and target
+    chip.summary()                            # print results summary
+    chip.show()                               # show layout
