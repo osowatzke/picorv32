364e337fca18e2e1e8afdf5f7c6d64a2818cc691
khadkaxashish <khadkaxashish@gmail.com>
2025-05-04 14:45:55 -0700

    build fpu with picorv32 and add its build report

---
+++ picorv32_with_fpu.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+from siliconcompiler import Chip
+from siliconcompiler.targets import skywater130_demo
+import argparse
+
+def main(signoff_only):
+
+    # Step 1: RTL2GDS
+    chip = Chip('picorv32')
+    chip.set('option', 'flow', 'asicflow')
+    chip.set('option', 'jobname', 'rtl2gds')
+
+    if not signoff_only:
+        chip.input("picorv32_with_fpu.v")
+        chip.input('picorv32.v')
+        chip.input('fpu/floating_point_add.v')
+        chip.input('fpu/floating_point_multiply.v')
+        chip.input('fpu/fmuls.v')
+        chip.input('fpu/fpu.v')
+
+        chip.clock('clk', period=25)
+        chip.set('option', 'remote', True)
+        chip.set('option', 'nodisplay', True)
+        chip.set('option', 'clean', True)
+
+        chip.use(skywater130_demo)
+        chip.run()
+        chip.summary()
+        chip.snapshot()
+
+    gds_path = chip.find_result('gds', step='write.gds')
+    vg_path = chip.find_result('vg', step='write.views')
+
+    # Step 2: Signoff Run
+    chip = Chip('picorv32')
+
+    chip.input(gds_path)
+    chip.input(vg_path)
+
+    chip.set('option', 'flow', 'signoffflow')
+    chip.set('option', 'jobname', 'signoff')
+    chip.set('option', 'remote', True)
+    chip.set('option', 'nodisplay', True)
+    chip.set('option', 'clean', True)
+
+    chip.use(skywater130_demo)
+    chip.run()
+    chip.summary()
+    chip.snapshot()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--signoff-only', action='store_true')
+    args = parser.parse_args()
+    main(args.signoff_only)
---
+++ picorv32_with_fpu.v
@@ -0,0 +1,52 @@
+`timescale 1 ns / 1 ps
+
+module picorv32_with_fpu (
+        input clk, resetn,
+        output reg trap,
+
+        // Look-Ahead Interface
+        output            mem_la_read,
+        output            mem_la_write,
+        output     [31:0] mem_la_addr,
+        output reg [31:0] mem_la_wdata,
+        output reg [ 3:0] mem_la_wstrb,
+
+        // Pico Co-Processor Interface (PCPI)
+        output reg        pcpi_valid,
+        output reg [31:0] pcpi_insn,
+        output     [31:0] pcpi_rs1,
+        output     [31:0] pcpi_rs2,
+        input             pcpi_wr,
+        input      [31:0] pcpi_rd,
+        input             pcpi_wait,
+        input             pcpi_ready,
+
+        // IRQ Interface
+        input      [31:0] irq,
+        output reg [31:0] eoi,
+
+        // Trace Interface
+        output reg        trace_valid,
+        output reg [35:0] trace_data
+);
+
+    // Memory signals.
+    reg mem_valid, mem_instr, mem_ready;
+    reg [31:0] mem_addr;
+    reg [31:0] mem_wdata;
+    reg [ 3:0] mem_wstrb;
+    reg [31:0] mem_rdata;
+
+    // No 'ready' signal in sky130 SRAM macro; presumably it is single-cycle?
+    always @(posedge clk)
+        mem_ready <= mem_valid;
+
+    // (Signals have the same name as the picorv32 module: use '.*' to autofill)
+    // Enable FPU
+    picorv32 #(
+        .ENABLE_FPU(1),
+        .ENABLE_PCPI(1))
+    rv32_soc (.*);
+
+   
+endmodule
